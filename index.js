677061061ff502dd1e246efe489229fa07f007cb
afgmlff <aeciofgm@gmail.com>
2023-03-08 19:46:48 -0300

    changed proxy to solve cors

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const axios = require('axios');
 const app = express()
 
+const cors = require("cors")
+
+app.use(cors())
+
 app.use(express.json());
 
 app.get('/data', (req, res) => {
